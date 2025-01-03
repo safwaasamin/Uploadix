d1f0e6ca8caf194263068c89ad08c5568cbf9b49
safwaasamin <safwaasamin@gmail.com>
2025-01-03 09:40:10 +0530

    commit dbconnection 4

--- dbconnection.js
+++ dbconnection.js
@@ -29,20 +29,12 @@ const mongoose = require("mongoose");
 
 const connectdb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 30000,
-            socketTimeoutMS: 30000,
-        });
-
-        mongoose.set('bufferCommands', false);
-        mongoose.set('strictQuery', false);
-
-        console.log("Database connected successfully!");
+        await mongoose.connect('mongodb+srv://safwaasamin:WilD1234@cluster0.kuc2p.mongodb.net/demo_first?retryWrites=true&w=majority');
+        console.log("Database connected");
     } catch (err) {
         console.error("Database connection error:", err);
     }
 };
 
+// Export the connection function
 module.exports = connectdb;
