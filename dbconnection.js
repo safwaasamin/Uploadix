f57a79d7edc5a1876fceacf75a558cd88a95d682
safwaasamin <safwaasamin@gmail.com>
2025-01-03 10:33:06 +0530

    commit

--- dbconnection.js
+++ dbconnection.js
@@ -1,30 +1,3 @@
-// // const mongoose= require("mongoose") 
-// // const connectdb=async()=>{
-// //   try{
-// // const connect=await mongoose.connect(process.env.connectionstring);
-// // console.log("connection successful",connect.connection.host,connect.connection.name)
-// //   }  catch(err) {
-// //     console.log(err);
-// //     process.exit(1);
-
-// //   }
-// // };
-// // module.exports=connectdb;
-
-// require('dotenv').config();  // Load environment variables
-// const mongoose = require('mongoose');
-
-// const connectdb = () => {
-//   const connectionString = process.env.MONGODB_URI;
-
-//   mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('Database connected'))
-//     .catch(err => console.error('Database connection error:', err));
-// };
-
-// // Export the connectdb function
-// module.exports = connectdb;
-
 const mongoose = require("mongoose");
 
 const connectdb = async () => {
